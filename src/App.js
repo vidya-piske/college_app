a449ebf0624b50e3131fcadbbdb0fbd3dfef2381
vidya-piske <vidyapiske123@gmail.com>
2024-05-07 15:33:45 +0530

    Updated component name from Department to DepartmentTable in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Semester from './components/Semester';
 import Faculty from './components/Faculty';
 import DepartmentTable from './components/Department';
-import StudentPage from './components/Student';
+import StudentTable from './components/Student';
 import Dashboard from './components/Dashboard';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Dashboard/>}/>
-        <Route path='/student' element={<StudentPage/>}/>
+        <Route path='/student' element={<StudentTable/>}/>
         <Route path='/department' element={<DepartmentTable/>}/>
         <Route path='/semester' element={<Semester/>}/>
         <Route path='/faculty' element={<Faculty/>}/>
